e239b182537139b7e2605ec5b21bea7c16d3b805
Alberto Lopez <Alberto2k7@gmail.com>
2020-03-30 16:20:43 -0300

    fix(sdk36): Fix support for Expo SDK36

--- src/defaults/persist.native.js
+++ src/defaults/persist.native.js
@@ -2,7 +2,7 @@
 
 // $FlowIgnore
 import { persistStore } from 'redux-persist';
-import AsyncStorage from 'react-native';
+import AsyncStorage from 'react-native'; // Supporting Expo SDK36
 
 export default (store: any, options: {}, callback: any) =>
   persistStore(store, { storage: AsyncStorage, ...options }, callback);
