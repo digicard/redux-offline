4e63a35017d7415752c74ca04ab0940e30e84b98
Alberto Lopez <Alberto2k7@gmail.com>
2020-03-30 16:15:14 -0300

    Update persist.native.js

--- src/defaults/persist.native.js
+++ src/defaults/persist.native.js
@@ -2,7 +2,7 @@
 
 // $FlowIgnore
 import { persistStore } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage'; // eslint-disable-line
+import AsyncStorage from 'react-native';
 
 export default (store: any, options: {}, callback: any) =>
   persistStore(store, { storage: AsyncStorage, ...options }, callback);
